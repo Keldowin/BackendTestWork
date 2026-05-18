f7f12335a08550327c83dd3921da67de50b5dbe2
Keldowin <ipeta2703@gmail.com>
2026-05-18 17:18:00 +0200

    Update loggining system

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,8 +1,34 @@
 import {config} from "../config/index.js";
+import {type ILogger, LogLevel} from "./logger.types.js";
 
 // Простая функция логгера
 // В будущем можно масштабировать до отдельных логгеров
 
-export function logger(message: string): void {
-    console.log(`[${config.appName}] ${message}`);
+export class Logger implements ILogger {
+    send(message: string, requestId?: string, logLevel: LogLevel = LogLevel.INFO): void {
+        const timestamp: number = Date.now();
+
+        if (requestId === undefined) {
+            console.log(`[${config.appName} / ${timestamp}] ${logLevel} | ${message}`);
+        } else {
+            console.log(`[${config.appName} / ${timestamp}] ${logLevel} | ${requestId} - ${message}`);
+        }
+    }
+
+    // Alias для разного уровня логирования
+    debug(message: string, requestId?: string): void {
+        this.send(message, requestId, LogLevel.DEBUG);
+    }
+    trace(message: string, requestId?: string): void {
+        this.send(message, requestId, LogLevel.TRACE)
+    }
+    info(message: string, requestId?: string): void {
+        this.send(message, requestId, LogLevel.INFO)
+    }
+    warn(message: string, requestId?: string): void {
+        this.send(message, requestId, LogLevel.WARN)
+    }
+    error(message: string, requestId?: string): void {
+        this.send(message, requestId, LogLevel.ERROR)
+    }
 }
